db0b2172db34d24ded8d1da2a1ea50db10d1e6b3
Shalitha Suranga <shalithasuranga@gmail.com>
2018-04-26 21:43:27 +0530

    List students from entries.json

--- scripts/homepage.js
+++ scripts/homepage.js
@@ -1,2 +1,33 @@
-let elem = document.querySelector('.sidenav');
-let instance = M.Sidenav.init(elem, null);
+$(document).ready(() => {
+    $('.sidenav').sidenav();
+    loadStudents();
+});
+
+
+let loadStudents = () => {
+
+    $.ajax({
+        url : 'storage/entries.json',
+        dataType : 'json',
+        type : 'GET',
+        success : (response) => {
+            renderStudents(response);
+        }
+    });
+
+};
+
+let renderStudents = (students) => {
+    let elem = $('#studentsLoader');
+    let con = '';
+    for(let i in students) {
+        let student = students[i];
+        con += '<div class="row">' +
+                    '<div class="s1">' + 
+                        '<h4>' + student.name + '</h4>' +
+                        '<a href="#">@' + student.githubUsername + '</a>' +
+                    '</div>' +
+                  '</div>';
+        elem.html(con);
+    }
+};
